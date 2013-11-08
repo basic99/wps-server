55e28f1d2ca426c77e6274e3c9b33d22491607a0
Jim White <jbw2003@earthlink.net>
2013-11-08 10:36:09 -0500

    update server docs

--- mywsgiref.py
+++ mywsgiref.py
@@ -1,39 +1,10 @@
+#!/usr/local/pythonenvs/ncthreatsenv/bin/python
+
 """
-#!/bin/bash
-#
-#wsgiref    This is the init script for starting up the wsgiref Pyton app server
-#               server.
-#
-# chkconfig: 2345 20 95
-# description: wsgi web app
+Create virtualenv with needed packages and python version.
+Use Supervisor (http://supervisord.org/) to run server.
+Configure proxy on web server to forward /wps to server.
 
-
-export PATH=/usr/local/pythonenvs/ncthreatsenv/bin:$PATH
-
-
-case "$1" in
-	start)
-		echo -n "Starting wsgiref services: "
-		python /var/www/wsgi/wps-server/mywsgiref.py & > /var/log/httpd/wsgiref_log  2>&1
-	;;
-	stop)
-		echo -n "Shutting down wsgiref services: "
-		pkill -f mywsgiref.py
-		
-	;;
-
-	restart)
-		echo -n "restarting wsgiref services "
-		$0 stop
-		$0 start
-
-	;;
-
-	*)
-		echo "Usage:  {start|stop|restart}"
-		exit 1
-	;;
-esac
 """
 
 
