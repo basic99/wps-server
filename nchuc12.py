51e163da8ec6943f3329069339a8b51c071a6549
Jim White <jbw2003@earthlink.net>
2013-11-21 15:44:33 -0500

    add some logging and comments

--- nchuc12.py
+++ nchuc12.py
@@ -1,18 +1,34 @@
+"""Module contains a class to perform GIS calculations."""
 import psycopg2
 from flask import  g
 import xml.dom.minidom
 import hashlib
 import time
 import random
+import logging
+import os
+
+cwd = os.path.dirname(os.path.realpath(__file__))
+logging.basicConfig(filename=cwd + '/logs/logs.log', 
+                    level=logging.INFO,    
+                    format='%(asctime)s %(levelname)s:%(message)s', 
+                    datefmt='%m/%d/%Y %I:%M:%S %p')
 
 class NCHuc12():
+
+    """Make GIS calculations to get huc12s.
+
+    Input GML file and text description. Run method 
+    execute to do calculations. 
+
+    """
 
     def __init__(self):
         self.gml = ''
         self.aoi_desc = ''
 
     def mkgeom(self):
-
+        """ Convert GML into list of Well-Known Text representations."""
         geom_list = list()
         dom = xml.dom.minidom.parseString(self.gml)
         polygons = dom.getElementsByTagName("gml:Polygon")
@@ -21,9 +37,11 @@ class NCHuc12():
                 gml_fragment =  polygon.toxml()
                 cur.execute("select st_astext(st_geomfromgml(%s))", (gml_fragment,))
                 geom_list.append( cur.fetchone()[0] )
+        logging.debug("returning %s polygons as WKT" % len(polygons))
         return geom_list
-    
+
     def calculations(self, aoiname):
+        """Placeholder for actual calculations to be added later. """
         hucs = list()
         with g.db.cursor() as cur:
             cur.execute("select pk from results where identifier = %s", (aoiname,))
@@ -34,8 +52,27 @@ class NCHuc12():
                 cur.execute("update results set resultcode = %s where pk = %s", 
                             (level, pk))
             g.db.commit()
-       
+
     def execute(self):
+        """Function to run calculations, called from wps.py.
+        
+        Call mkgeom to convert GML to list of polygons as WKT. 
+        Create identifier using time, random and md5.
+        Insert into table aoi row for each polygon of input.
+        Call stored procedure with identifier to calculate overlap with 
+        huc12 layer and update table results with rows for each huc12.
+        Calculate extents of huc12s. 
+        Add row to table aoi_results string representation of huc12s, 
+        extent, and identifier, returning the id of inserted row.
+        
+        Returns:
+        ident - generated md5 string
+        aoi_id - id of row in table aoi_results for this aoi
+        extent - list of extents for huc12 for this aoi
+        
+        """
+        logging.info("aoi description is %s" % self.aoi_desc)
+        logging.debug(self.gml[:1000])
         huc12s = list()
         input_geoms = self.mkgeom()
         digest = hashlib.md5()
@@ -46,7 +83,11 @@ class NCHuc12():
             for b in input_geoms:
                 cur.execute("insert into aoi(identifier, the_geom) values (%s, ST_GeomFromText(%s, 4326))"
                             , (ident , b))
+            #Stored PL/PGSQL procedure. Use PostGIS to calculate overlaps.
+            #Add row to table results for each huc12 with identifier.
+            #Identifier column is used by geoserver cql filter.
             cur.execute("select aoitohuc(%s)", (ident,))
+            #insert random results
             self.calculations(ident)
             cur.execute("select huc12 from results where identifier = %s", (ident,))
             for row in cur:
@@ -65,9 +106,12 @@ class NCHuc12():
             aoi_id = cur.fetchone()[0]
             g.db.commit()
             extent = [xmin, ymin, xmax, ymax]
-            
+            logging.info("md5 identifier is %s" % ident)
+            logging.info("pk in table aoi_results is %s" % aoi_id)
+            logging.info("extent of huc12s is %s, %s, %s, %s" % 
+                         (extent[0], extent[1], extent[2], extent[3]))
+
         return (ident, aoi_id, extent)
-        
 
 
 
--- wps.py
+++ wps.py
@@ -1,3 +1,14 @@
+"""Main file to run Flask app.
+
+The web app is a GIS app that uses REST architecture.
+It is based loosely on the concept of a WPS(Web Processing Service),
+but does not comply with that standard.
+Create a resource that is a list of huc12s(12 digit hydrologic units)
+that overlap the input geometry which is input as GML file.
+Allow access to this resource.
+
+"""
+
 from flask import Flask
 from flask import request
 from flask import url_for
@@ -30,7 +41,14 @@ def teardown_request(exception):
         
 @app.route('/wps', methods=['POST', ])
 def post_aoi():
+    """Create a new resource with post request.
     
+    Create nchuc12 object to make the calculations and 
+    pass the post data of gml and text description.
+    Return identifier and extent as json, an http response of 201
+    and location header with resource location.
+    
+    """
     huc = nchuc12.NCHuc12()
     huc.aoi_desc = request.form['text']
     huc.gml = request.form['gml']
@@ -45,6 +63,7 @@ def post_aoi():
 
 @app.route('/wps/<int:id>', methods=['GET',])
 def resource_aoi(id):
+    """Method to get resource of huc12s returned as a web page. """
     with g.db.cursor(cursor_factory=psycopg2.extras.DictCursor) as cur:
         cur.execute("select * from aoi_results where pk = %s", (id, ))
         rec = cur.fetchone()
